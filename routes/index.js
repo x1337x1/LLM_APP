94eee91712295672f823dc2dea45a66625ddb4b4
mohammed <hachemimohamed99@gmail.com>
2023-10-30 21:25:15 +0100

    authentication & authorization

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const accountsRoutes = require("./account.routes")
-const homeRoutes = require("./home.routes")
 
 
 
@@ -9,7 +8,7 @@ const homeRoutes = require("./home.routes")
 
 
 router.use("/accounts/v1", accountsRoutes);
-router.use("/home/v1", homeRoutes);
+
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
